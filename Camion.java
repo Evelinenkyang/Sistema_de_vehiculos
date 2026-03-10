71faadb86114459af53e20e68544741eaa96d1e4
evelinenkyang <20253rd059@utez.edu.mx>
2026-03-10 00:13:29 -0600

    Camion.java

--- Camion.java
+++ Camion.java
@@ -2,7 +2,7 @@ public class Camion extends Vehiculo{
     protected int capacidadCarga;
     protected int numeroEjes;
 
-    public Camion(String marca, String modelo, int anio, double precio, int capacidadCarga, int numeroEjes);{
+    public Camion(String marca, String modelo, int anio, double precio, int capacidadCarga, int numeroEjes){
 
         super(marca,modelo,anio,precio);
         this.capacidadCarga = capacidadCarga;
